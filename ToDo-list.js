9eba9db0a90ac35529de07ac8fe0a1315e9e71d9
Rafa <rrl89@hotmail.com>
2018-04-02 21:04:06 +0200

    setting functions of all buttons

--- ToDo-list.js
+++ ToDo-list.js
@@ -4,6 +4,10 @@ $("#task").keypress(insertTask);
 
 var tasks = $("#listToDo");
 var tasksDone =  $("#listDone");
+$("#rmAll").on("click", removeAll);
+$("#rmToDo").on("click", removeToDo);
+$("#rmDoneList").on("click", removeDone);
+
 
 function insertTask(event){
 	if (event.keyCode===13){
@@ -11,9 +15,10 @@ function insertTask(event){
 	event.preventDefault();
 	var newTask = document.createElement("li");
 	$(newTask).text(task); ///////////////poner dolar porque sin dolar no funciona 
-	$(newTask).addClass("item");
-	$(newTask).prop("id", "toDo");
-	$(newTask).on("click", changeTask)
+	$(newTask).addClass("item toDo");
+
+	//$(newTask).prop("class", "toDo");
+	$(newTask).on("click", changeTask);
 	tasks.append(newTask);
 	$("#task").val("");
 	}
@@ -21,17 +26,30 @@ function insertTask(event){
 
 function changeTask(e){
 	var checkTask = e.target;
-	if (checkTask.id==="toDo"){
+	if ($(checkTask).hasClass("toDo")){
+		$(checkTask).removeClass("toDo");
+		$(checkTask).addClass("already-done");
 		tasksDone.append(checkTask);
-		checkTask.id = "Done";
 	} else {
 		tasks.append(checkTask);
-		checkTask.id = "toDo";
+		$(checkTask).addClass("toDo");
+		//checkTask.id = "toDo";
+		$(checkTask).removeClass("already-done");
 	}
 }
 
+function removeAll(){
+	$(".already-done").remove();
+	$(".toDo").remove();
+}
 
+function removeToDo(){
+	$(".toDo").remove();
+}
 
+function removeDone(){
+	$(".already-done").remove();
+}
 
 	
 });
